e97975945bf47f23c3355b2164dbd5ddbdf960d4
DaNiELChIoRo <>
2021-05-25 13:04:10 -0500

    first commit

---
+++ golden_cross.py
@@ -0,0 +1,234 @@
+
+# Raw Package
+from numpy.core.numeric import NaN
+#Data Source
+import yfinance as yf
+#Data viz
+import plotly.graph_objs as go
+import functools
+import datetime
+import  sched, time
+from bitso_requester import get_account_balance, get_tricker, place_order
+
+s = sched.scheduler(time.time, time.sleep)
+sleep = 60 * 10
+def main():
+    print("Doing stuff...")
+    # do your stuff
+    s.enter(0, 1, make_calculus, (s,))
+    s.run()  
+
+def make_calculus(sc=s):
+    print("making calculations")
+    s.enter(sleep, 1, make_calculus, (sc, ))
+    calculate('BTC-USD')
+    calculate('ETH-USD')
+    calculate('XRP-USD')
+    calculate('MANA-USD')
+    calculate('LTC-USD')
+
+def trade(currency, action):
+    #TODO:- When buy or sell cross marked place a stop loss and limited order for the crest!.
+    balances = get_account_balance()
+    if action == 'BUY':
+        fiat_currency = filter(lambda x: x["currency"] == "mxn", balances)
+        # print('## fiat currency: ', fiat_currency)
+        balance = float(fiat_currency[0]['available'])
+        print("user account currency balance: ", balance)
+        if balance > 0:
+            # gettin' the last trade prices for that asset!
+            prices = get_last_transactions(currency + "_mxn")
+            if prices:
+                # TRyin' to apply saffe rules
+                budget = balance/(2 * 4)
+                # calculation the amount of crypto per budget.
+                amount = float("{:.5f}".format(budget/prices[1])) #balance/(2 * 4)
+                #Getting the highest price to buy
+                price = prices[0]
+                print("$$$$$$ he order to ", action, " will be place with price ", price, " and amount of cryptos", amount, "\n")
+                # placing the order!
+                place_order(amount, price, side=action.lower())
+    elif action == 'SELL':
+        cripto_currency = filter(lambda x: x["currency"] == currency, balances)
+        print('## cripto currency: ', cripto_currency)
+        if cripto_currency:
+            balance = float(cripto_currency['available'])
+            print("user account currency balance: ", balance)
+            if balance > 0:
+                # gettin' the last trade prices for that asset!
+                prices = get_last_transactions(currency + "_mxn")
+                if prices:
+                    # Tryin' to apply saffe rules
+                    budget = balance/(2 * 4)
+                    # calculation the amount of crypto per budget.
+        else:
+            return Exception('No crypto assets available for transaction!!')
+
+def calculate(pair, period = '8d', interval = '90m', show = True):
+#Importing market data
+    data = yf.download(tickers=pair, period = period, interval = interval)
+
+    #Adding Moving average calculated field
+    data['MA5'] = data['Close'].rolling(5).mean()
+    data['MA20'] = data['Close'].rolling(20).mean()
+    time = datetime.datetime.now() - datetime.timedelta(minutes=90)
+    # print("current time ", datetime.datetime.now() , " and minus 90 min ", time)
+    
+    crosses = []
+    for idx, item in enumerate(data['MA5']):        
+        if idx == 0 or idx == len(data) or data['MA20'][idx] == NaN or item == NaN:
+            continue
+        if data['MA20'][idx] == item:
+            print('NEVER TO BE CALLED ***** cross point: ', item)   
+            crosses.append((data.index[idx], data['MA20'][idx]))          
+        if idx + 1 < len(data['MA5']):   
+            low_long_range = functools.reduce(lambda x, y: x + y, data['MA20'][idx - 1:idx + 1])
+            low_short_range = functools.reduce(lambda x, y: x + y, data['MA5'][idx - 1:idx + 1])
+            long_long_range = functools.reduce(lambda x, y: x + y, data['MA20'][idx:idx + 2])
+            long_short_range = functools.reduce(lambda x, y: x + y, data['MA5'][idx:idx + 2])
+            if low_long_range > low_short_range and long_long_range < long_short_range:
+                # print('***** cross point: ', item , ' and ', data.index[idx].timestamp())   
+                crosses.append((data.index[idx], data['MA20'][idx], "BUY"))                
+                continue
+            elif low_long_range < low_short_range and long_long_range > long_short_range:
+                # print('***** cross point: ', item, 'and ' , data.index[idx].timestamp()) 
+                crosses.append((data.index[idx], data['MA20'][idx], "SELL"))                
+                continue             
+    
+    #Deciding if buying or selling
+    last_cross_time = crosses[-1][0]
+    current_less_90_timestamp = to_seconds(time)
+    
+    # Validating 
+    if last_cross_time.timestamp() >= current_less_90_timestamp:
+        # TODO:- call to action
+        action = crosses[-1][2]
+        currency = pair.partition("-")[0].lower()
+        print("YOU SHOULD ", action, "!!!")             
+        trade(currency=currency, action=action)
+    else:
+        print("Too long for to act!!")
+
+    #declare figure
+    fig = go.Figure()
+
+    #Title
+    fig.update_layout(title_text=pair)
+
+    #Candlestick
+    fig.add_trace(go.Candlestick(x=data.index,
+                    open=data['Open'],
+                    high=data['High'],
+                    low=data['Low'],
+                    close=data['Close'], name = 'market data'))
+
+    #Add Moving average on the graph
+    fig.add_trace(go.Scatter(x=data.index, y= data['MA20'],line=dict(color='blue', width=1.5), name = 'Long Term MA'))
+    fig.add_trace(go.Scatter(x=data.index, y= data['MA5'],line=dict(color='orange', width=1.5), name = 'Short Term MA'))
+    for idx, cross in enumerate(crosses):
+        # print('cross: ', cross, ' x: ', crosses_index[idx])
+        fig.add_annotation(x=cross[0],
+                            y=cross[1],
+                        # text="intersect",
+                        text = cross[2],
+                        font=dict(family="sans serif",
+                                    size=18,
+                                    color="green"),                        
+                        ax=0,
+                        ay=-100,
+                        showarrow=True,
+                        arrowcolor='purple',
+                        arrowwidth=3,
+                        arrowhead=1)
+
+    #Updating X axis and graph
+    # X-Axes
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=3, label="3d", step="day", stepmode="backward"),
+                dict(count=5, label="5d", step="day", stepmode="backward"),
+                dict(count=7, label="WTD", step="day", stepmode="todate"),
+                dict(step="all")
+            ])
+        )
+    )
+
+    #Show
+    if show:
+        fig.show()
+
+def to_seconds(date):
+    return float(time.mktime(date.timetuple()))
+
+# def _line_intersection(line1, line2):
+#     intersections = []
+#     print('line1: ', len(line1))
+#     print('line1 0: ', len(line1[0]))
+#     print('line1 1: ', len(line1[1]))
+#     print('line2: ', len(line2))
+#     print('line2 0: ', len(line2[0]))
+#     print('line2 1: ', len(line2[1]))
+#     for idx, _ in enumerate(line1[0]):
+#         if idx + 1 < len(line1[0]):
+#             if line1[1][idx] == NaN or line2[1][idx] == NaN:
+#                 continue
+#             print('loop ', idx, ' line 1_0: ', line1[0][idx], ' line2_0: ', line2[0][idx], ' line 1_1: ', line1[1][idx], ' line2_1: ', line2[1][idx])                        
+#             xdiff = (line1[0][idx] - line1[1][idx], line2[0][idx] - line2[1][idx])
+#             ydiff = (line1[0][idx + 1] - line1[1][idx + 1], line2[0][idx + 1] - line2[1][ idx + 1])
+
+#             def det(a, b):
+#                 return a[0] * b[1] - a[1] * b[0]
+
+#             def _det(a, b):
+#                 print('a: ', len(a), ' b: ', len(b))
+#                 return a[idx] * b[idx + 1] - a[idx + 1] * b[idx]
+
+#             div = det(xdiff, ydiff)
+#             if div == 0:
+#                 raise Exception('lines do not intersect')
+
+#             d = (_det(*line1), _det(*line2))
+#             x = det(d, xdiff) / div
+#             y = det(d, ydiff) / div
+#             print('x: ', x, 'y: ', y, ' div: ', div, ' d: ', d)
+#             intersections.append((x, y ))
+#     return intersections
+
+# def line(p1, p2):
+#     A = (p1[1] - p2[1])
+#     B = (p2[0] - p1[0])
+#     C = (p1[0]*p2[1] - p2[0]*p1[1])
+#     return A, B, -C
+
+# def intersection(L1, L2):
+#     D  = L1[0] * L2[1] - L1[1] * L2[0]
+#     Dx = L1[2] * L2[1] - L1[1] * L2[2]
+#     Dy = L1[0] * L2[2] - L1[2] * L2[0]
+#     if D != 0:
+#         x = Dx / D
+#         y = Dy / D
+#         return x,y
+#     else:
+#         return False
+
+# def line_intersection(line1, line2):
+#     xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+#     ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+#     def det(a, b):
+#         return a[0] * b[1] - a[1] * b[0]
+
+#     div = det(xdiff, ydiff)
+#     if div == 0:
+#         raise Exception('lines do not intersect')
+
+#     d = (det(*line1), det(*line2))
+#     x = det(d, xdiff) / div
+#     y = det(d, ydiff) / div
+#     # print('x: ', x, 'y: ', y, ' div: ', div, ' d: ', d)
+#     return x, y
+
+if __name__ == "__main__":
+    main()
---
+++ services/bitso_requester.py
@@ -0,0 +1,220 @@
+import time
+import hmac
+import hashlib
+import requests
+import json
+from decimal import Decimal
+from requests.api import request
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#  Define your request
+bitso_key = os.getenv("bitso_key")
+bitso_secret = os.getenv("bitso_secret")
+request_path="/v3/balance/"
+
+def main():
+    print("Calling requests")    
+    print("account balance:", get_account_balance() )
+    get_user_trades()
+    get_available_books()
+    get_tricker()
+    get_last_transactions()
+    print("\n $$$$$\tuser open orders: ", get_open_orders())
+    print("\n\n")
+    __test_(action='SELL')
+
+def __test_(currency = "eth", action = 'BUY'):
+    """
+        Internal test for placeing orders!.
+    """
+    balances = get_account_balance()
+    if action == 'BUY':
+        fiat_currency = filter(lambda x: x["currency"] == "mxn", balances)
+        # print('## fiat currency: ', fiat_currency)
+        balance = float(fiat_currency[0]['available'])
+        print("user account currency balance: ", balance)
+        if balance > 0:
+            # gettin' the last trade prices for that asset!
+            prices = get_last_transactions(currency + "_mxn")
+            if prices:
+                # TRyin' to apply saffe rules
+                budget = 100 #balance/(2 * 4)
+                # calculation the amount of crypto per budget.
+                amount = float("{:.5f}".format(budget/prices[1])) #balance/(2 * 4)
+                #Getting the highest price to buy
+                price = prices[0]
+                print("$$$$$$ he order to ", action, " will be place with price ", price, " and amount of cryptos", amount, "\n")
+                # placing the order!
+                place_order(amount, price, side=action.lower())
+    elif action == 'SELL':
+        cripto_currency = filter(lambda x: x["currency"] == currency, balances)
+        print('## cripto currency: ', cripto_currency)
+        if cripto_currency:
+            balance = float(cripto_currency['available'])
+            print("user account currency balance: ", balance)
+            if balance > 0:
+                # gettin' the last trade prices for that asset!
+                prices = get_last_transactions(currency + "_mxn")
+                if prices:
+                    # Tryin' to apply saffe rules
+                    budget = balance/(2 * 4)
+                    # calculation the amount of crypto per budget.
+        else:
+            return Exception('No crypto assets available for transaction!!')
+# Create signature
+def __getSignature__(http_method = 'GET', request_path = '/v3/balance/', parameters={}):
+    nonce =  str(int(round(time.time() * 1000)))
+    message = nonce+http_method+request_path
+    if (http_method == "POST"):
+        message += json.dumps(parameters)
+    return hmac.new(bitso_secret.encode('utf-8'),
+                                                message.encode('utf-8'),
+                                                hashlib.sha256).hexdigest()
+
+def __make_request__(path, params = {}, type = 'GET'):
+    nonce =  str(int(round(time.time() * 1000)))
+    request_path = path
+    signature = __getSignature__(request_path=request_path)
+    # Build the auth header
+    auth_header = 'Bitso %s:%s:%s' % (bitso_key, nonce, signature)  
+    if type == 'GET':
+        response = requests.get("https://api.bitso.com" + request_path, headers={"Authorization": auth_header})
+    elif type == 'POST':
+        response = requests.post("https://api.bitso.com" + request_path, json = params, headers={"Authorization": auth_header})
+    result = json.loads(response.content)    
+    if result["success"]:
+        return result["payload"]   
+
+def get_account_balance():
+    r""" 
+        Retrieves the user's account NON ZERO balance in the differente assets he/she has.        
+    """
+    result = __make_request__(path=request_path)
+    
+    payload = result["balances"]
+    # print("balance payload: ", payload)        
+    balance = filter(lambda x: float(x["available"]) > 0, payload)
+    return balance
+
+def get_user_trades():
+    """
+        Returns the user's trades
+    """
+
+    path = '/v3/user_trades/'
+    result = __make_request__(path)    
+    return result
+
+def get_open_orders(book = 'btc_mxn'):
+    r"""
+        Retrieves the user's open orders from the given book
+    """
+    
+    path = '/v3/open_orders?book=btc_mxn'
+    result = __make_request__(path)
+    
+    return result
+
+def cancel_order(order_id = "all"):
+    """
+        Cancels the given order_id's order which coud be one or an array of ids
+
+        @param $order_id: orders?oids=<oid>,<oid>,<oid>
+
+    """
+    path = '/v3/orders/' + order_id
+    request = __make_request__(path, type='DELETE');
+    
+    return request
+
+def get_available_books(book_name = 'eth_mxn'):
+    r""" 
+        Returns the list of availbale books (cryptos) to trade 
+
+        example: [u'btc_mxn', u'eth_btc', u'eth_mxn', u'xrp_btc']
+    """
+    nonce =  str(int(round(time.time() * 1000)))
+    request_path = '/v3/available_books/'
+    signature = __getSignature__(request_path=request_path)
+    # Build the auth header
+    auth_header = 'Bitso %s:%s:%s' % (bitso_key, nonce, signature)  
+
+    response = requests.get("https://api.bitso.com" + request_path, headers={"Authorization": auth_header})    
+    result = json.loads(response.content)
+    # print("available books response: ", result)
+    if result["success"]:
+        payload = result['payload']
+        books = map(lambda x: x["book"], payload)
+        book = filter(lambda x: x["book"] == book_name, payload)[0]
+        print("\n\n %%book ", book_name, ": ", book, "\n\n")
+        print("\n$$$ min price for ", book_name, ": ", book["minimum_price"], " and max price: ", book["maximum_price"], " average: \n")
+        print("books: ", books)
+        return books
+
+def get_tricker(book = 'btc_mxn'):
+    r"""
+        Retrieves the tricker information from the given book
+        returns a tuple with the highest price to buy and the lowest price to sell
+        (high, low)
+    """
+
+    request_path = '/v3/ticker/?book=' + book
+    payload = __make_request__(request_path)
+        
+    high = float(payload["bid"])
+    low = float(payload["ask"])
+    print('highest buy: ', high, ' lowest sell: ', low)
+    return ("{:.2f}".format(high), "{:.2f}".format(low))
+
+def get_last_transactions(book = 'btc_mxn'):
+    r"""
+        Retrieves the traiding information from the given book
+        returns a tuple with the highest price to buy and the lowest price to sell
+        (high, low)
+    """
+
+    request_path = '/v3/order_book/?book=' + book
+
+    payload = __make_request__(request_path)    
+    # print("last trades payload: ", payload)
+    low = payload["bids"][0]
+    high = payload["asks"][0]
+
+    min_ask = float(str(min(map(lambda x: x["price"], payload["asks"]))))
+    max_bid = float(str(max(map(lambda x: x["price"], payload["bids"]))))
+    delta = min_ask - max_bid
+    print("max_bid: ", max_bid, " min_ask: ", min_ask)
+    print('highest buy: ', max_bid, ' lowest sell: ', min_ask, " fair price: ", delta)
+    return (max_bid, min_ask, delta)
+
+def place_order(amount, price, book = "btc_mxn", side = "BUY", type = 'limit', time_in_force = 'immediateorcancel'):
+    """
+        Place an order into the 
+        @param $amount
+    """
+    nonce =  str(int(round(time.time() * 1000)))
+    request_path = '/v3/orders/'
+    #params
+    params = {
+        'book': book,
+        'side': side,
+        "type": type,
+        'major': str(amount),
+        'price': str(price),
+        # 'time_in_force': time_in_force
+    }
+    signature = __getSignature__(http_method="POST", request_path=request_path, parameters=params)
+    # Build the auth header
+    auth_header = 'Bitso %s:%s:%s' % (bitso_key, nonce, signature)  
+
+    response = requests.post("https://api.bitso.com" + request_path, json = params, headers={"Authorization": auth_header})
+    print("response: ", response.content)
+    result = json.loads(response.content)
+    if result["success"]:
+        return result["payload"]
+
+if __name__ == "__main__":
+    main()
